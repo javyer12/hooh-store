dba4681898c371153a0bb3cf628e6b1d2112fabc
javyer <javyermurillo1@gmail.com>
2021-11-13 01:31:23 -0600

    cambios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import '../styles/components/app.css';
 
-const App = () => <h1>hola mundo</h1>;
+const App = () => {
+
+    <h1>hola mundo</h1>;
+    <Link to="/">Home</Link>
+
+}
+
 
 export default App;
