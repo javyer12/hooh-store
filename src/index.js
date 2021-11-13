a830b6284d7a6aa74d154efc6c5536deebba30a5
javyer <javyermurillo1@gmail.com>
2021-11-13 01:58:05 -0600

    delete file

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('home'));
+ReactDOM.render(<App />, document.getElementById('app'));
